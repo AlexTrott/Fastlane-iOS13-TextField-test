5cc84c182e4a37deaa527b6906034e99f2d133cf
Alex Trott <hello@alextrott.me>
2019-12-02 19:59:40 +0000

    Adds exaple files

--- TextFieldUITests/TextFieldUITests.swift
+++ TextFieldUITests/TextFieldUITests.swift
@@ -17,27 +17,20 @@ class TextFieldUITests: XCTestCase {
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func test_example() {
 
-    func testExample() {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        app.textFields.firstMatch.tap()
+        app.keys["h"].tap()
+        app.keys["e"].tap()
+        app.keys["l"].tap()
+        app.keys["l"].tap()
+        app.keys["o"].tap()
     }
 
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
